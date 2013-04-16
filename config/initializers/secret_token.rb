c460ad245c5145bf6b09f632cb48d7a55d60a934
Gavin Ching <gavinchingy@gmail.com>
2013-04-16 01:48:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GavinBlog::Application.config.secret_token = 'e29e1283f09b2db60c0d7003526d5ca968785a40486bcac3a31209d53d3b29103cb46e5d2e58a1153a290e617f7f93a10d066cff4252dea73f8c429d1373a807'
