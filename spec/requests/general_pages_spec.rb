d6eddf856be604165551201f00dea5b966b4f152
Gavin Ching <gavinchingy@gmail.com>
2013-04-24 01:26:52 -0400

    Changed a bit.

--- spec/requests/general_pages_spec.rb
+++ spec/requests/general_pages_spec.rb
@@ -8,14 +8,14 @@ describe "General Pages" do
   describe "Home page" do
     before { visit root_path } 
 
-    it { should have_selector('h1',    text: "Gavin Ching's Journey") }
+    it { should have_selector('h1',    text: "Posts") }
     it { should have_selector('title', text: full_title('')) }
   end
 
   describe "About page" do
   	before {visit about_path}
 
-  	it {should have_selector('h1',   text: "About Gavin Ching")}
+  	it {should have_selector('h2',   text: "About")}
   	it { should have_selector('title', text: full_title('About')) }
 
   end
@@ -23,7 +23,7 @@ describe "General Pages" do
   describe "Contact Page" do
   	before {visit contact_path}
 
-  	it {should have_selector('h1',   text: "Contact")}
+  	it {should have_selector('h2',   text: "Contact")}
   	it { should have_selector('title', text: full_title('Contact')) }
 
   end
