22e59ac88afc040b8750920c46d2569122e1f3d5
Gavin Ching <gavinchingy@gmail.com>
2013-04-23 23:42:22 -0400

    Fix layout of site.

--- app/helpers/general_pages_helper.rb
+++ app/helpers/general_pages_helper.rb
@@ -2,7 +2,16 @@ module GeneralPagesHelper
 
 	def wrap(content)
     sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
-  	end
+	end
+  	
+
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(options)
+    gravatar_id = Digest::MD5::hexdigest("gavinchingy@gmail.com")
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: "gavin", class: "gravatar")
+  end
 
 
 private
